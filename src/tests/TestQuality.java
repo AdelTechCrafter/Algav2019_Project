3c3a41044f85ad31f58a54995e33d65fc338aea8
El Amraoui Adel <Elamraoui.Adel@hotmail.com>
2020-01-13 13:37:14 +0100

    generate results with tests.TestQuality.java and tests.TestTime.java

--- src/tests/TestQuality.java
+++ src/tests/TestQuality.java
@@ -1,4 +1,4 @@
-package resultats;
+package tests;
 
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.ChartUtilities;
@@ -17,33 +17,31 @@ import algorithms.Stats;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.category.DefaultCategoryDataset;
 
-public class TestTime extends ApplicationFrame {
+public class TestQuality extends ApplicationFrame {
 
-  
-
-/**
+   /**
 	 * 
 	 */
-	private static final long serialVersionUID = -7623812800959203271L;
+	private static final long serialVersionUID = 1L;
 
-public TestTime( String applicationTitle , String chartTitle ) {
+public TestQuality( String applicationTitle , String chartTitle ) {
       super(applicationTitle);
       JFreeChart lineChart = ChartFactory.createLineChart(
          chartTitle,
-         "Instance of Test File","Execution time (microseconds) ",
+         "Instance of Test File","Quality",
          createDataset(),
          PlotOrientation.VERTICAL,
          true,true,false);
          
       ChartPanel chartPanel = new ChartPanel( lineChart );
-      chartPanel.setPreferredSize( new java.awt.Dimension( 1000 , 1000 ) );
+      chartPanel.setPreferredSize( new java.awt.Dimension( 1200 , 600 ) );
       setContentPane( chartPanel );
    }
 
    private static DefaultCategoryDataset createDataset( ) {
       DefaultCategoryDataset dataset = new DefaultCategoryDataset( );
-      Map<Integer,Double> resRitter= Stats.timeCircleSamples();
-      Map<Integer,Double> resToussaint= Stats.timeRectangleSamples();
+      Map<Integer,Double> resRitter= Stats.qualityCircleSamples();
+      Map<Integer,Double> resToussaint= Stats.qualityRectangleSamples();
       int i=0;
       int j=0;
       for (Map.Entry<Integer,Double> entry : resRitter.entrySet()) {
@@ -61,9 +59,9 @@ public TestTime( String applicationTitle , String chartTitle ) {
    }
    
    public static void main( String[ ] args ) throws IOException {
-      TestTime chart = new TestTime(
-         "VaroumasSamples execution time" ,
-         "Execution time of each sample: Ritter vs Toussaint (Varoumas dataset)");
+      TestQuality chart = new TestQuality(
+         "VaroumasSamples Quality" ,
+         "Quality of each sample: Ritter vs Toussaint (Varoumas dataset)");
 
       chart.pack( );
       RefineryUtilities.centerFrameOnScreen( chart );
@@ -72,13 +70,13 @@ public TestTime( String applicationTitle , String chartTitle ) {
 	   //générer le fichier
 	   DefaultCategoryDataset line_chart_dataset = createDataset();
 	   JFreeChart lineChartObject = ChartFactory.createLineChart(
-		         "Execution time of each sample: Ritter vs Toussaint (Varoumas dataset)","Instance of Test File(1664)",
-		         "Execution time (microseconds) ",
+		         "Quality of each sample: Ritter vs Toussaint (Varoumas dataset)","Instance of Test File(1664)",
+		         "Quality",
 		         line_chart_dataset,PlotOrientation.VERTICAL,
 		         true,true,false);
-      int width = 1000;    /* Width of the image */
-      int height = 1000;   /* Height of the image */ 
-      File lineChart = new File( "results/TimeResults.jpeg" ); 
+      int width = 1200;    /* Width of the image */
+      int height = 600;   /* Height of the image */ 
+      File lineChart = new File( "results/QualityResults.jpeg" ); 
       ChartUtilities.saveChartAsJPEG(lineChart ,lineChartObject, width ,height);
    }
 }
--- src/tests/TestTime.java
+++ src/tests/TestTime.java
@@ -1,4 +1,4 @@
-package resultats;
+package tests;
 
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.ChartUtilities;
@@ -17,31 +17,33 @@ import algorithms.Stats;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.category.DefaultCategoryDataset;
 
-public class TestQuality extends ApplicationFrame {
+public class TestTime extends ApplicationFrame {
 
-   /**
+  
+
+/**
 	 * 
 	 */
-	private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = -7623812800959203271L;
 
-public TestQuality( String applicationTitle , String chartTitle ) {
+public TestTime( String applicationTitle , String chartTitle ) {
       super(applicationTitle);
       JFreeChart lineChart = ChartFactory.createLineChart(
          chartTitle,
-         "Instance of Test File","Quality",
+         "Instance of Test File","Execution time (microseconds) ",
          createDataset(),
          PlotOrientation.VERTICAL,
          true,true,false);
          
       ChartPanel chartPanel = new ChartPanel( lineChart );
-      chartPanel.setPreferredSize( new java.awt.Dimension( 1200 , 600 ) );
+      chartPanel.setPreferredSize( new java.awt.Dimension( 1000 , 1000 ) );
       setContentPane( chartPanel );
    }
 
    private static DefaultCategoryDataset createDataset( ) {
       DefaultCategoryDataset dataset = new DefaultCategoryDataset( );
-      Map<Integer,Double> resRitter= Stats.qualityCircleSamples();
-      Map<Integer,Double> resToussaint= Stats.qualityRectangleSamples();
+      Map<Integer,Double> resRitter= Stats.timeCircleSamples();
+      Map<Integer,Double> resToussaint= Stats.timeRectangleSamples();
       int i=0;
       int j=0;
       for (Map.Entry<Integer,Double> entry : resRitter.entrySet()) {
@@ -59,9 +61,9 @@ public TestQuality( String applicationTitle , String chartTitle ) {
    }
    
    public static void main( String[ ] args ) throws IOException {
-      TestQuality chart = new TestQuality(
-         "VaroumasSamples Quality" ,
-         "Quality of each sample: Ritter vs Toussaint (Varoumas dataset)");
+      TestTime chart = new TestTime(
+         "VaroumasSamples execution time" ,
+         "Execution time of each sample: Ritter vs Toussaint (Varoumas dataset)");
 
       chart.pack( );
       RefineryUtilities.centerFrameOnScreen( chart );
@@ -70,13 +72,13 @@ public TestQuality( String applicationTitle , String chartTitle ) {
 	   //générer le fichier
 	   DefaultCategoryDataset line_chart_dataset = createDataset();
 	   JFreeChart lineChartObject = ChartFactory.createLineChart(
-		         "Quality of each sample: Ritter vs Toussaint (Varoumas dataset)","Instance of Test File(1664)",
-		         "Quality",
+		         "Execution time of each sample: Ritter vs Toussaint (Varoumas dataset)","Instance of Test File(1664)",
+		         "Execution time (microseconds) ",
 		         line_chart_dataset,PlotOrientation.VERTICAL,
 		         true,true,false);
-      int width = 1200;    /* Width of the image */
-      int height = 600;   /* Height of the image */ 
-      File lineChart = new File( "results/QualityResults.jpeg" ); 
+      int width = 1000;    /* Width of the image */
+      int height = 1000;   /* Height of the image */ 
+      File lineChart = new File( "results/TimeResults.jpeg" ); 
       ChartUtilities.saveChartAsJPEG(lineChart ,lineChartObject, width ,height);
    }
 }
